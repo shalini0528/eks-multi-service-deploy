3143b625e6826973319c33884473653453a8a188
Shalini Kularathne <shalikularathne93@gmail.com>
2025-07-10 10:22:03 +0530

    mysql file changes

--- game-service/index.js
+++ game-service/index.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'lugxdb'
+  database: process.env.DB_NAME || 'lugxdbGame'
 });
 
 //get all games
